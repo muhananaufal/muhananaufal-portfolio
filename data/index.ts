152379b9b3c994ddb4df86617f123b7945fec459
muhananaufal <muhananaufal8@gmail.com>
2024-06-05 11:19:22 +0700

    Create portfolio version 1

---
+++ data/index.ts
@@ -0,0 +1,204 @@
+export const navItems = [
+	{ name: 'About', link: '#about' },
+	{ name: 'Projects', link: '#myProjects' },
+	{ name: 'Testimonials', link: '#testimonials' },
+	{ name: 'Contact', link: '#contact' },
+];
+
+export const gridItems = [
+	{
+		id: 1,
+		title: 'I prioritize client collaboration, fostering open communication ',
+		description: '',
+		className: 'lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]',
+		imgClassName: 'w-full h-full',
+		titleClassName: 'justify-end',
+		img: '/b2.jpg',
+		spareImg: '',
+	},
+	{
+		id: 2,
+		title: "I'm very flexible with time zone communications",
+		description: '',
+		className: 'lg:col-span-2 md:col-span-3 md:row-span-2',
+		imgClassName: '',
+		titleClassName: 'justify-start',
+		img: '',
+		spareImg: '',
+	},
+	{
+		id: 3,
+		title: 'My tech stack',
+		description: 'I constantly try to improve',
+		className: 'lg:col-span-2 md:col-span-3 md:row-span-2',
+		imgClassName: '',
+		titleClassName: 'justify-center',
+		img: '',
+		spareImg: '',
+	},
+	{
+		id: 4,
+		title: 'Tech enthusiast with a passion for development.',
+		description: '',
+		className: 'lg:col-span-2 md:col-span-3 md:row-span-1',
+		imgClassName: '',
+		titleClassName: 'justify-start',
+		img: '/grid.svg',
+		spareImg: '/b4.svg',
+	},
+
+	{
+		id: 5,
+		title: 'Currently building a MERN Stack Application',
+		description: 'The Inside Scoop',
+		className: 'md:col-span-3 md:row-span-2',
+		imgClassName: 'absolute right-0 bottom-0 md:w-96 w-60',
+		titleClassName: 'justify-center md:justify-start lg:justify-center',
+		img: '/b5.svg',
+		spareImg: '/grid.svg',
+	},
+	{
+		id: 6,
+		title: 'Do you want to start a project together?',
+		description: '',
+		className: 'lg:col-span-2 md:col-span-3 md:row-span-1',
+		imgClassName: '',
+		titleClassName: 'justify-center md:max-w-full max-w-60 text-center',
+		img: '',
+		spareImg: '',
+	},
+];
+
+export const myProjects = [
+	{
+		id: 1,
+		title: 'Project 1',
+		des: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		img: '/p0.svg',
+		iconLists: ['/mongodb.svg', '/nodejs.svg', '/reactjs.svg', '/tailwind.svg', '/framermotion.svg'],
+		link: '/github.com/muhananaufal',
+	},
+	{
+		id: 2,
+		title: 'Project 2',
+		des: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		img: '/p0.svg',
+		iconLists: ['/typescript.svg', '/nextjs.svg', '/framermotion.svg', '/threejs.svg', '/tailwind.svg'],
+		link: '/github.com/muhananaufal',
+	},
+];
+
+export const testimonials = [
+	{
+		quote:
+			'Working with Muhana Naufal has been a game-changer for our project. Their expertise as a Full Stack Web Developer, particularly with the MERN Stack has brought our vision to life. Muhana Naufal seamlessly navigated both front-end and back-end challenges, ensuring a smooth and efficient development process. Their problem-solving skills and attention to detail have greatly enhanced the functionality and user experience of our web application. I highly recommend Muhana Naufal for any web development needs.',
+		name: 'Nasywan Damar',
+		title: 'Web Developer',
+	},
+	{
+		quote:
+			'Working with Muhana Naufal has been a game-changer for our project. Their expertise as a Full Stack Web Developer, particularly with the MERN Stack has brought our vision to life. Muhana Naufal seamlessly navigated both front-end and back-end challenges, ensuring a smooth and efficient development process. Their problem-solving skills and attention to detail have greatly enhanced the functionality and user experience of our web application. I highly recommend Muhana Naufal for any web development needs.',
+		name: 'Nasywan Damar',
+		title: 'Web Developer',
+	},
+	{
+		quote:
+			'Working with Muhana Naufal has been a game-changer for our project. Their expertise as a Full Stack Web Developer, particularly with the MERN Stack has brought our vision to life. Muhana Naufal seamlessly navigated both front-end and back-end challenges, ensuring a smooth and efficient development process. Their problem-solving skills and attention to detail have greatly enhanced the functionality and user experience of our web application. I highly recommend Muhana Naufal for any web development needs.',
+		name: 'Nasywan Damar',
+		title: 'Web Developer',
+	},
+	{
+		quote:
+			'Working with Muhana Naufal has been a game-changer for our project. Their expertise as a Full Stack Web Developer, particularly with the MERN Stack has brought our vision to life. Muhana Naufal seamlessly navigated both front-end and back-end challenges, ensuring a smooth and efficient development process. Their problem-solving skills and attention to detail have greatly enhanced the functionality and user experience of our web application. I highly recommend Muhana Naufal for any web development needs.',
+		name: 'Nasywan Damar',
+		title: 'Web Developer',
+	},
+	{
+		quote:
+			'Working with Muhana Naufal has been a game-changer for our project. Their expertise as a Full Stack Web Developer, particularly with the MERN Stack has brought our vision to life. Muhana Naufal seamlessly navigated both front-end and back-end challenges, ensuring a smooth and efficient development process. Their problem-solving skills and attention to detail have greatly enhanced the functionality and user experience of our web application. I highly recommend Muhana Naufal for any web development needs.',
+		name: 'Nasywan Damar',
+		title: 'Web Developer',
+	},
+];
+
+export const companies = [
+	{
+		id: 1,
+		name: 'cloudinary',
+		img: '/cloud.svg',
+		nameImg: '/cloudName.svg',
+	},
+	{
+		id: 2,
+		name: 'appwrite',
+		img: '/app.svg',
+		nameImg: '/appName.svg',
+	},
+	{
+		id: 3,
+		name: 'HOSTINGER',
+		img: '/host.svg',
+		nameImg: '/hostName.svg',
+	},
+	{
+		id: 4,
+		name: 'stream',
+		img: '/s.svg',
+		nameImg: '/streamName.svg',
+	},
+	{
+		id: 5,
+		name: 'docker.',
+		img: '/dock.svg',
+		nameImg: '/dockerName.svg',
+	},
+];
+
+export const workExperience = [
+	{
+		id: 1,
+		title: 'Network Engineer Intern',
+		desc: 'Contributing to the design and optimization of network infrastructures to improve performance and reliability.',
+		className: 'md:col-span-2',
+		thumbnail: '/exp1.svg',
+	},
+	{
+		id: 2,
+		title: 'Volunteer Web Mentor',
+		desc: 'Guiding students in building web projects using MERN Stack to foster their coding skills and creativity.',
+		className: 'md:col-span-2', // change to md:col-span-2
+		thumbnail: '/exp2.svg',
+	},
+	//   {
+	//     id: 3,
+	//     title: "Freelance App Dev Project",
+	//     desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+	//     className: "md:col-span-2", // change to md:col-span-2
+	//     thumbnail: "/exp3.svg",
+	//   },
+	//   {
+	//     id: 4,
+	//     title: "Lead Frontend Developer",
+	//     desc: "Developed and maintained user-facing features using modern frontend technologies.",
+	//     className: "md:col-span-2",
+	//     thumbnail: "/exp4.svg",
+	//   },
+];
+
+export const socialMedia = [
+	{
+		id: 1,
+		img: '/git.svg',
+		link: 'https://github.com/muhananaufal',
+	},
+	{
+		id: 2,
+		img: '/ig.svg',
+		link: 'https://www.instagram.com/_muhananaufal_',
+	},
+	{
+		id: 3,
+		img: '/link.svg',
+		link: 'https://www.linkedin.com/in/muhana-naufal-al-atsari/',
+	},
+];
